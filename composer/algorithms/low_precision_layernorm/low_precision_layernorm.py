aafe81dcb6afa5ecb98518eeb7595980f1cc8b0c
Alex Trott <alex@mosaicml.com>
2022-10-19 11:24:39 -0700

    Add argument to turn on forced fp16 in LPLN

--- composer/algorithms/low_precision_layernorm/low_precision_layernorm.py
+++ composer/algorithms/low_precision_layernorm/low_precision_layernorm.py
@@ -23,36 +23,41 @@ from composer.utils import module_surgery
 log = logging.getLogger(__name__)
 
 
-def _cast_if_autocast_enabled(hidden_states):
+def _cast_if_autocast_enabled(hidden_states, force_fp16=False):
     if not torch.is_autocast_enabled():
         return hidden_states
     else:
-        return torch.cuda.amp.autocast_mode._cast(hidden_states, torch.get_autocast_gpu_dtype())  
-        # return torch.cuda.amp.autocast_mode._cast(hidden_states, torch.float16) # <-- Use this instead to hardcode to fp16
+        if force_fp16:
+            return torch.cuda.amp.autocast_mode._cast(hidden_states, torch.float16) # <-- Use this instead to hardcode to fp16
+        else:
+            return torch.cuda.amp.autocast_mode._cast(hidden_states, torch.get_autocast_gpu_dtype())  
 
 
 class CastLayerNorm(torch.nn.LayerNorm):
 
-    def __init__(self, normalized_shape, eps, elementwise_affine):
+    def __init__(self, normalized_shape, eps, elementwise_affine, force_fp16=False):
         super().__init__(normalized_shape=normalized_shape, eps=eps, elementwise_affine=elementwise_affine)
+        self.force_fp16 = bool(force_fp16)
 
     def forward(self, x):
         module_device = x.device
-        downcast_x = _cast_if_autocast_enabled(x)
-        downcast_weight = _cast_if_autocast_enabled(self.weight)
-        downcast_bias = _cast_if_autocast_enabled(self.bias)
+        downcast_x = _cast_if_autocast_enabled(x, self.force_fp16)
+        downcast_weight = _cast_if_autocast_enabled(self.weight, self.force_fp16)
+        downcast_bias = _cast_if_autocast_enabled(self.bias, self.force_fp16)
         with torch.autocast(enabled=False, device_type=module_device.type):
             return F.layer_norm(downcast_x, self.normalized_shape, downcast_weight, downcast_bias, self.eps)
 
 
-def from_Layer(layer: torch.nn.LayerNorm, idx: int) -> CastLayerNorm:
+def from_Layer(layer: torch.nn.LayerNorm, idx: int, force_fp16: bool) -> CastLayerNorm:
     del idx
-    return CastLayerNorm(layer.normalized_shape, layer.eps, layer.elementwise_affine)
+    return CastLayerNorm(layer.normalized_shape, layer.eps, layer.elementwise_affine, force_fp16=force_fp16)
 
 
-def apply_low_precision_layernorm(model, optimizers: Union[torch.optim.Optimizer, Sequence[torch.optim.Optimizer]]):
+def apply_low_precision_layernorm(model, optimizers: Union[torch.optim.Optimizer, Sequence[torch.optim.Optimizer]], force_fp16=False):
 
-    policy: Dict[Type[torch.nn.Module], module_surgery.ReplacementFunction] = {torch.nn.LayerNorm: from_Layer}
+    policy: Dict[Type[torch.nn.Module], module_surgery.ReplacementFunction] = {
+        torch.nn.LayerNorm: lambda module, idx: from_Layer(module, idx, force_fp16),
+    }
 
     replaced_instances = module_surgery.replace_module_classes(module=model, optimizers=optimizers, policies=policy)
     if len(replaced_instances) == 0:
@@ -64,9 +69,10 @@ def apply_low_precision_layernorm(model, optimizers: Union[torch.optim.Optimizer
 
 class LowPrecisionLayerNorm(Algorithm):
 
-    def __init__(self):
-        # LowPrecisionLayerNorm takes no arguments
-        pass
+    def __init__(self, force_fp16: bool = False):
+        self.force_fp16 = force_fp16
+        # # LowPrecisionLayerNorm takes no arguments
+        # pass
 
     @property
     def find_unused_parameters(self) -> bool:
@@ -79,4 +85,4 @@ class LowPrecisionLayerNorm(Algorithm):
 
     def apply(self, event: Event, state: State, logger: Logger) -> Optional[int]:
         del event, logger  # unused
-        apply_low_precision_layernorm(model=state.model, optimizers=state.optimizers)
+        apply_low_precision_layernorm(model=state.model, optimizers=state.optimizers, force_fp16=self.force_fp16)
